bc37cfa5d108d39dc262bb97f99881e5218776f0
kurt <kurta.tomas@gmail.com>
2021-03-02 20:57:00 +0100

    final

---
+++ index.php
@@ -0,0 +1,429 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Zadanie</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    
+    <style>
+    @font-face {
+    src:url('assets/fonts/DMSans-Regular.ttf') format('truetype');
+    font-family: 'DM Sans';
+    font-style: normal; 
+    font-weight: normal;
+    } 
+    div {
+        font-family: 'DM Sans', sans-serif;
+        font-weight: normal;
+        font-style: normal;
+        }
+    </style>
+
+</head> 
+<body>
+
+<div>
+
+    
+    
+<!-- HEADER -->
+
+    
+    
+<main class="homepage">
+    
+    <header class="post-header">
+    
+                
+    <div class="container">
+                    
+        <div class="nav-bar">
+			
+                <div id="mainWrapper">
+				<h1 class="logo">
+					<a href="index.php">
+                        <img id="logo" src="assets/img/logo.svg" alt="$logo">
+					</a>
+				</h1>
+				<nav class="group">
+                    
+					<ul class="menu navigation">
+                        
+            <li><a href="index.php">Úvod</a></li>
+            <li><a href="#ktosme">Kto&nbsp;sme</a></li>
+            <li><a href="#referencie">Referencie</a></li>
+            <li><a href="#galeria">Galéria</a></li>
+            <li><a href="#contact">Kontakt</a></li>
+		
+					</ul>
+                    
+				</nav>
+            </div>
+            
+		</div>
+        </div>  
+        
+        <section id="uvod">
+            
+            <table>
+  
+                <tr>
+                <th>
+                    <p7>Výnimočné,<br>exotické jedlo</p7>
+                </th>
+                </tr>
+                        
+                    <tr>
+                    <th>
+                    <h2>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+                    </h2><br><br>
+                    </th>
+                    </tr>
+                        
+                    <tr>
+                    <td>
+                        <a href="#ktosme"><button class="btn uvod">O nás</button></a> 
+                        <a href="#contact"><button class="btn uvodd">Kontaktujte nás</button></a>
+                    </td>
+                    
+                    </tr>
+                    
+                </table>
+           
+        </section>
+       
+     </header>
+       
+</main>
+			
+    <div class="containerrr">
+                    <div class ="one"></div>
+                    <div class ="two"></div>
+                    <div class ="three"></div>           
+    </div>
+    
+    
+    
+    
+    
+<!-- KTO SME -->
+    
+    
+    
+    
+    
+<section id="ktosme">
+    
+                <div class="containerr">
+                    
+                    <div class ="left"></div>
+                    <div class ="right"></div>
+                   
+                </div>
+    
+                
+                <div class="container">
+                    
+                <table class="kto">
+                
+                
+  
+                <tr class='sme'>
+                <th class='sme' colspan="5">
+                    <h2>KTO SME</h2>
+                    <p1>Lívia a Viktória</p1>
+                </th>
+                </tr>
+                        
+                    <tr class='sme'>
+                    <th class='sme' colspan="5"><h7>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.<br><br>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</h7><br><br>
+                    </th>
+                    </tr>
+                        
+                    <tr class='sme'>
+                    <td><h2> Firemné eventy </h2></td>
+                    <td><h2> Svadby </h2></td>
+                    <td><h2> Spoločenské podujatia </h2></td>
+                    <td><h2> Plesy </h2></td>
+                    <td><h2> Gala večere </h2></td>
+                    </tr>
+                    
+                </table> 
+                        
+                
+              </div>
+            
+</section>    
+
+    
+    
+    
+    
+    
+    
+<!-- GALERIA -->
+    
+
+    
+    
+    
+    
+    
+    
+<section id="galeria">
+    
+        <div class="container">
+
+                    <h7>GALÉRIA</h7><br>
+                    <p6>Zážitkový catering</p6>
+
+        </div>
+    
+</section>
+    
+<div class="galerie">
+
+<section>
+    <div class="rt-container">
+          <div class="col-rt-12">
+              <div class='demo-container'>
+                  <div class='carousel'>
+                    <input class='carousel__activator' id='carousel-slide-activator-1' name='carousel' type='radio' checked='checked'>
+                    <input class='carousel__activator' id='carousel-slide-activator-2' name='carousel' type='radio'>
+                    <input class='carousel__activator' id='carousel-slide-activator-3' name='carousel' type='radio'>
+                    <input class='carousel__activator' id='carousel-slide-activator-4' name='carousel' type='radio'>
+                    <input class='carousel__activator' id='carousel-slide-activator-5' name='carousel' type='radio'>
+                    
+                    <div class='carousel__controls'>
+                      <label class='carousel__control carousel__control--forward' for='carousel-slide-activator-2'>
+                      </label>
+                    </div>
+                    <div class='carousel__controls'>
+                      <label class='carousel__control carousel__control--forward' for='carousel-slide-activator-3'>
+                      </label>
+                    </div>
+                    <div class='carousel__controls'>
+                      <label class='carousel__control carousel__control--forward' for='carousel-slide-activator-4'>
+                      </label>
+                    </div>
+                    <div class='carousel__controls'>
+                      <label class='carousel__control carousel__control--forward' for='carousel-slide-activator-5'>
+                      </label>
+                    </div>
+                    <div class='carousel__controls'>
+                      <label class='carousel__control carousel__control--forward' for='carousel-slide-activator-1'>
+                      </label>
+                    </div>
+                      
+                      
+                    <div class='carousel__screen'>
+                      <div class='carousel__track'>
+                          
+                        <div class='carousel__item carousel__item--mobile-in-1 carousel__item--tablet-in-2 carousel__item--desktop-in-3'>
+                          <div class='demo-content'>
+                            
+                            <img id="myImg" class="myImg" src="assets/img/gallery/galeria-1.jpg" alt="galeria1">
+        
+                          </div>
+                        </div>
+                          
+                        <div class='carousel__item carousel__item--mobile-in-1 carousel__item--tablet-in-2 carousel__item--desktop-in-3'>
+                          <div class='demo-content'>
+                              
+                            <img id="myImg" class="myImg" src="assets/img/gallery/galeria-2.jpg" alt="galeria2">
+                              
+                          </div>
+                        </div>
+                          
+                        <div class='carousel__item carousel__item--mobile-in-1 carousel__item--tablet-in-2 carousel__item--desktop-in-3'>
+                          <div class='demo-content'>
+                            <img id="myImg" class="myImg" src="assets/img/gallery/galeria-3.jpg" alt="galeria3">
+                          </div>
+                        </div>
+                        <div class='carousel__item carousel__item--mobile-in-1 carousel__item--tablet-in-2 carousel__item--desktop-in-3'>
+                          <div class='demo-content'>
+                            <img id="myImg" class="myImg" src="assets/img/gallery/galeria-4.jpg" alt="galeria4">
+                          </div>
+                        </div>
+                        <div class='carousel__item carousel__item--mobile-in-1 carousel__item--tablet-in-2 carousel__item--desktop-in-3'>
+                          <div class='demo-content'>
+                            <img id="myImg" class="myImg" src="assets/img/gallery/galeria-5.jpg" alt="galeria5">
+                          </div>
+                        </div>
+                          <div class='carousel__item carousel__item--mobile-in-1 carousel__item--tablet-in-2 carousel__item--desktop-in-3'>
+                          <div class='demo-content'>
+                            <img id="myImg" class="myImg" src="assets/img/gallery/galeria-1.jpg" alt="galeria1">
+                          </div>
+                        </div>
+                          <div class='carousel__item carousel__item--mobile-in-1 carousel__item--tablet-in-2 carousel__item--desktop-in-3'>
+                          <div class='demo-content'>
+                            <img id="myImg" class="myImg" src="assets/img/gallery/galeria-2.jpg" alt="galeria2">
+                          </div>
+                        </div>
+                          <div class='carousel__item carousel__item--mobile-in-1 carousel__item--tablet-in-2 carousel__item--desktop-in-3'>
+                          <div class='demo-content'>
+                            <img id="myImg" class="myImg" src="assets/img/gallery/galeria-3.jpg" alt="galeria3">
+                          </div>
+                        </div>                          
+                        </div>
+                        
+                                <!-- The Modal -->
+                                <div id="myModal" class="modal">
+                                  <span class="close">&times;</span>
+                                  <img class="modal-content" id="img01">
+                                  <div id="caption"></div>
+                                </div>
+
+                                <script>
+                                // Get the modal
+                                var modal = document.getElementById("myModal");
+
+                                // Get the image and insert it inside the modal - use its "alt" text as a caption
+                                var img = document.getElementsByClassName('myImg');
+                                var modalImg = document.getElementById("img01");
+                                var captionText = document.getElementById("caption");
+
+                                var showModal = function(){
+                                  modal.style.display = "block";
+                                  modalImg.src = this.src;
+                                  captionText.innerHTML = this.alt;
+                                }
+
+                                for (var i = 0; i < img.length; i++) {
+                                    img[i].addEventListener('click', showModal);
+                                }
+
+                                // Get the <span> element that closes the modal
+                                var span = document.getElementsByClassName("close")[0];
+
+                                // When the user clicks on <span> (x), close the modal
+                                span.onclick = function() { 
+                                  modal.style.display = "none";
+                                }
+                                </script>
+                        
+                      </div>
+                </div>                  
+            </div>
+        </div>
+    </div>
+</section>
+        
+</div>
+    
+    
+    
+    
+<!-- REFERENCIE -->
+    
+    
+    
+    
+    
+<section id="referencie">
+    
+<div class="container">
+
+    <table class="ref">
+
+        <tr class='ren'>
+        <td class='ren'>
+            <h4>REFERENCIE</h4>
+            <p3>Povedali o nás</p3>
+        </td>
+
+        <td rowspan="2">
+        <p5>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.<br><br>Lorem Ipsum</p5>
+            </td>
+        <td rowspan="2">
+          <p5>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.<br><br>Lorem Ipsum</p5>
+          </td>
+        <td rowspan="2">
+          <p5>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.<br><br>Lorem Ipsum</p5>
+            </td>
+        </tr>
+
+        <tr class='ren'>
+        <td class='ren' style="text-align:center">
+            <button class="btn refer">Viac recenzií</button>
+        </td>
+        </tr>
+
+    </table>
+                    
+</div>
+
+</section>    
+    
+    
+    
+    
+<!-- KONTAKT -->
+    
+    
+    
+    
+<section id="contact">
+   
+        <article>
+
+            <section class="container contact-float-handler"> 
+                <a href="index.php">
+                    <img class="logoo" src="assets/img/logo.svg" alt="$logo">
+                </a>
+                <form action=""> 
+                    
+                        <h2>KONTAKT</h2><br>
+                        <p1>Napíšte nám</p1><br><br><br><br>
+                    
+                    <div class="form-main">
+                        <label for="name"><h7>Meno a priezvisko *</h7></label><br>
+                        <input type="name" id="name" name="name" value=""> <br>
+                    </div>
+
+                    <div class="form-main">
+                        <label for="telefon"><h7>Telefón *</h7></label><br>
+                        <input type="telefon" id="telefon" name="telefon" value=""> <br>
+                    </div>
+                    <br>
+                    <div class="form-main">
+                        <label for="email"><h7>Emailová adresa *</h7></label><br>
+                        <input type="email" id="email" name="email" value=""> <br>
+                    </div>
+                    <br>
+                    <div>
+                        <label for="text"><h7>Správa *</h7></label><br>
+                        <textarea id="message" name="message" rows="7" cols="77"></textarea> <br><br>
+                        <button class="btn kont">Odoslať</button> 
+                    </div>
+                    
+                    <div class="pattern"></div>
+                    
+                </form>
+            </section>
+
+        </article>
+
+</section>    
+ 
+
+    
+    
+<!-- FOOTER -->    
+    
+
+
+
+<footer>
+
+    <p9>© liviscatering.sk 2021</p9>
+
+</footer>
+
+    
+
+</div>
+    
+</body>
+</html>
